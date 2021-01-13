9312776f778f374a7dff94de90f159813bfedfd1
M-Stable <fchanghlee@gmail.com>
2021-01-13 21:04:45 +1300

    update svg path to fit wider screens

--- src/CustomComponents/BackgroundBlue.jsx
+++ src/CustomComponents/BackgroundBlue.jsx
@@ -3,11 +3,8 @@ import React from "react";
 function BackgroundBlue(props) {
   return (
     <svg
-      width="100%"
-      height="159"
-      // viewBox="0 0 100vw 159"
-      fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 1440 320"
       style={{
         position: "absolute",
         left: 0,
@@ -15,12 +12,12 @@ function BackgroundBlue(props) {
         bottom: 0,
         zIndex: "1",
       }}
-      preserveAspectRatio="xMaxYMin meet"
     >
       <path
-        d="M0 0.5C0 0.5 457 16.5 1065.5 102.5C1674 188.5 1920 0.5 1920 0.5V159H0V0.5Z"
         fill="#7798AB"
-      />
+        fill-opacity="1"
+        d="M0,128L80,144C160,160,320,192,480,218.7C640,245,800,267,960,261.3C1120,256,1280,224,1360,208L1440,192L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+      ></path>
     </svg>
   );
 }
